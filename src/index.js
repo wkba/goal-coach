9f7851bdf935d0d3bb85829bc6cbf7cf4b8647f8
若林俊輔 <waka@wakabayashishunsuke-no-MacBook-Pro.local>
2017-08-14 01:44:08 +0900

    Add link to signin & singout component"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import {Router, Route, browserHistory} from 'react-router';
+import {Route, BrowserRouter} from 'react-router-dom'
+
 import App from './components/App';
 import SingIn from './components/Signin';
 import SingUp from './components/SignUp';
 
 ReactDOM.render(
-    <div>App</div>, document.getElementById('root')
+    <BrowserRouter>
+        <div>
+            <Route exact = {true} path = '/' component = {App} />
+            <Route path='/signin' component={SingIn}/>
+            <Route path='/signup' component={SingUp}/>
+        </div>
+    </BrowserRouter>, document.getElementById('root')
 )
