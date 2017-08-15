e63f47391a8230ac562fec7ad30816fb7aaf0fd5
wakabayashi <wkba61@gmail.com>
2017-08-15 13:47:25 +0900

    ログイン状態を確認して、リダイレクトを設定する前

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import {Router, Route, browserHistory} from 'react-router';
 import {Route, BrowserRouter} from 'react-router-dom'
+
 
 //自作のコンポーネント
 import App from './components/App';
@@ -13,19 +13,18 @@ import {firebaseApp} from "./firebase"
 //全てのアクセスはこのファイルを上から順に通っているので、ここでログイン状態が確認できる。
 //ユーザがログインしているか判別してる
 firebaseApp.auth().onAuthStateChanged(user => {
-    if(user){
+    if (user) {
         console.log('user has signed in or up', user);
-    }else{
+    } else {
         console.log(('user has signed out or still needs to sign in.'))
     }
 })
 
-
 ReactDOM.render(
-    //各ページへのリンクを指定
+//各ページへのリンクを指定
     <BrowserRouter>
         <div>
-            <Route exact = {true} path = '/' component = {App} />
+            <Route exact={true} path='/' component={App}/>
             <Route path='/signin' component={SingIn}/>
             <Route path='/signup' component={SingUp}/>
         </div>
